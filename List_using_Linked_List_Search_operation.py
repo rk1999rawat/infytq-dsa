41a0b6e451e708141775cb97ece4df2d55ee02b6
rk1999rawat <44577532+rk1999rawat@users.noreply.github.com>
2020-05-11 00:16:15 +0530

    dsa day2

---
+++ List_using_Linked_List_Search_operation.py
@@ -0,0 +1,82 @@
+# infytq-dsa
+class Node:
+    def __init__(self,data):
+        self.__data=data
+        self.__next=None
+    def get_data(self):
+        return self.__data
+    
+    def set_data(self,data):
+        self.__data=data
+    
+    def get_next(self):
+        return self.__next
+    
+    def set_next(self,next_node):
+        self.__next=next_node
+    
+class LinkedList:
+    def __init__(self):
+        self.__head=None
+        self.__tail=None
+    
+    def get_head(self):
+        return self.__head
+    
+    def get_tail(self):
+        return self.__tail
+    
+    def add(self,data):
+        node=Node(data)
+        if self.__head==None:
+            self.__head=node
+            self.__tail=node
+        else:
+            temp=self.__tail
+            temp.set_next(node)
+            self.__tail=node
+        
+    
+    def display(self):
+        temp=self.__head
+        while temp != None:
+            print(temp.get_data())
+            temp=temp.get_next()
+        
+        #Remove pass and copy the code you had written to display the element(s).
+    
+    def find_node(self,data):
+        temp=self.get_head()
+        while temp!=None:
+            if temp.get_data()==data:
+                return temp
+            temp=temp.get_next()
+        return None    
+        
+        #Remove pass and write the logic to find the node and return the node if found or return None.
+                                            
+    #You can use the below __str__() to print the elements of the DS object while debugging
+    def __str__(self):
+        temp=self.__head
+        msg=[]
+        while(temp is not None):
+           msg.append(str(temp.get_data()))
+           temp=temp.get_next()
+        msg=" ".join(msg)
+        msg="Linkedlist data(Head to Tail): "+ msg
+        return msg
+
+
+list1=LinkedList()
+list1.add("Milk")
+list1.add("Sugar")
+list1.display()
+
+#Add all the required element(s)
+
+#Search for the required node
+node=list1.find_node("Sugar")
+if(node!=None):
+    print("Node found")
+else:
+    print("Node not found") 
